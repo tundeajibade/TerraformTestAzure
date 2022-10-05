14a541c80a3e868a81932e9ef476d49db1b2b7ea
Mohammed Ajibade <tundeajibade@gmail.com>
2022-10-05 22:31:25 +0200

    Modify bashscript

--- BashScripts.sh
+++ BashScripts.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#COnnect and set Subscription Context in Azure
+#Connect and set Subscription Context in Azure
 az login
-az account set --subscription "MPN - John Lunn"
-
+az account set --subscription 5c84dc43-439f-4f1e-bc44-629cf30c9037
 
 #Set Variables for Storage account and Key Vault that support the Terraform implementation
-RESOURCE_GROUP_NAME=jonnychipz-infra
-STORAGE_ACCOUNT_NAME=jonnychipztstate
+RESOURCE_GROUP_NAME=atm-infra
+STORAGE_ACCOUNT_NAME=atmtstate
 CONTAINER_NAME=tstate
 STATE_FILE="terraform.state"
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location uksouth
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
 
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
@@ -29,6 +28,6 @@ echo "access_key: $ACCOUNT_KEY"
 echo "state_file: $STATE_FILE"
 
 # Create KeyVault and example of storing a key
-az keyvault create --name "jonnychipzkv" --resource-group "jonnychipz-infra" --location uksouth
-az keyvault secret set --vault-name "jonnychipzkv" --name "tstateaccess" --value {$ACCOUNT_KEY}
-az keyvault secret show --vault-name "jonnychipzkv" --name "tstateaccess"
+az keyvault create --name "atmkv" --resource-group $RESOURCE_GROUP_NAME --location westeurope
+az keyvault secret set --vault-name "atmkv" --name "tstateaccess" --value {$ACCOUNT_KEY}
+az keyvault secret show --vault-name "atmkv" --name "tstateaccess"
